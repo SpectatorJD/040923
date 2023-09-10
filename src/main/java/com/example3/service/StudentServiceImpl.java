045538d5a5bac38f97f8ea2cbc537595634880d4
Tkakishak <tkakishakd@mail.ru>
2023-09-10 21:32:46 +0300

    1009

--- src/main/java/com/example3/service/StudentServiceImpl.java
+++ src/main/java/com/example3/service/StudentServiceImpl.java
@@ -5,6 +5,8 @@ import com.example3.model.Faculty;
 import com.example3.model.Student;
 import com.example3.model.StudentAge;
 import com.example3.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -14,35 +16,42 @@ public class StudentServiceImpl implements StudentService{
 
 
     private final StudentRepository studentRepository;
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public Student addStudent (Student student){
+        logger.info("Was invoked method for to add student");
         return studentRepository.save(student);
     }
 
     public Student findStudent(long id){
+        logger.info("Was invoked method for to find student by id");
         return studentRepository.findById(id).get();
     }
     @Override
     public Collection<Student> getByAge(Integer min, Integer max){
+        logger.info("Was invoked method for to get student by age");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     @Override
     public Student editStudent(long id, Student student) {
+        logger.info("Was invoked method for to edit student");
         return studentRepository.save(student);
     }
 
     @Override
     public void deleteStudent(long id) {
+        logger.info("Was invoked method for to delete student by id");
         studentRepository.deleteById(id);
     }
 
 
     public Faculty findFaculty(String name) {
+        logger.info("Was invoked method for to find student by faculty");
         return  studentRepository.findByName(name).getFaculty();
     }
 
@@ -53,10 +62,12 @@ public class StudentServiceImpl implements StudentService{
 
     @Override
     public StudentAge getAverageAge() {
+        logger.info("Was invoked method for to get students by average age");
         return studentRepository.getAvarageStudentAge();
     }
     @Override
     public Collection<Student> getTopFiveStudent() {
+        logger.info("Was invoked method for to get top five students");
         return studentRepository.getTopFiveStudent();
     }
 }
